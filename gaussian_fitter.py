554f548ceb0509dd37c415d2176908d492bb7e33
John Nagel <jnagel@iastate.edu>
2018-02-07 20:27:09 -0600

    Added best fit for vertical gaussian beam

--- gaussian_fitter.py
+++ gaussian_fitter.py
@@ -1,5 +1,6 @@
 import pdb
 import sys
+import glob
 
 import numpy as np
 from scipy.optimize import curve_fit
@@ -9,20 +10,43 @@ from matplotlib.collections import PatchCollection
 
 from util import gaussian_2d, gaussian_1d
 
-sys.path.insert(0, 'D:\\PycharmProjects\\THzProcClass')
+sys.path.insert(0, 'C:\\PycharmProjects\\THzProcClass')
 
 from THzData import THzData
 
-basedir = 'D:\\Work\\Pin Reflector'
-filename = 'Pin_mp_0.tvl'
+basedir = 'C:\\Work\\Pin Reflector\\Ordered Data'
+basefile = 'Pin_mp_'
 
 gate = [[0, 4096], [-175, 200]]
 
 radius = 0.75  # mm
 sigma = 1.25
 
-# load in the data file that was taken at the focus height
-data = THzData(filename, basedir, gate=gate, follow_gate_on=True, signal_type=1)
+z = np.linspace(-2.5, 2.5, 11)
+
+data_array = list()
+
+n_files = len(glob.glob(basedir + '\\*.tvl'))
+
+for i in range(n_files):
+    if i < 10:
+        filename = basefile + '0' + str(i) + '.tvl'
+    else:
+        filename = basefile + str(i) + '.tvl'
+
+    data_array.append(THzData(filename, basedir, gate=gate, signal_type=1))
+
+data = data_array[5]  # this is focus
+
+c_scans = np.zeros((n_files, data.y_step, data.x_step))
+
+for i, scan in enumerate(data_array):
+    c_scans[i] = scan.c_scan
+
+# find the maximum amplitude of each C-Scan, resulting array is n_files long
+max_amps = c_scans.max(axis=1).max(axis=1)
+
+normalized_c_scan = data.c_scan / data.c_scan.max()
 
 yy, xx = np.meshgrid(data.y, data.x, indexing='ij')
 
@@ -32,18 +56,18 @@ xdata = np.vstack((yy.flatten(), xx.flatten()))
 
 # need to determine what the approximate shape of C-Scan gaussian
 # initial guess for each of the arguments
-A = data.c_scan.max()
+A = 1
 x0 = 0
 y0 = 0
 sigma_x = 0.7
 sigma_y = 0.7
 p0 = (A, x0, y0, sigma_x, sigma_y)
 
-popt, pcov = curve_fit(gaussian_2d, xdata, data.c_scan.flatten(), p0)
+popt, pcov = curve_fit(gaussian_2d, xdata, normalized_c_scan.flatten(), p0)
 
 # change p0 to see what 2D gaussian looks like
-p0 = (1, 0, 0, sigma, sigma)
-made_c_scan = gaussian_2d(xdata, *p0).reshape(data.y_step, data.x_step)
+# p0 = (1, 0, 0, sigma, sigma)
+made_c_scan = gaussian_2d(xdata, *popt).reshape(data.y_step, data.x_step)
 
 x = np.linspace(-3, 3, 100)
 y = gaussian_1d(x, 1, 0, sigma)
@@ -54,7 +78,8 @@ patches.append(beam_spot)
 p = PatchCollection(patches, edgecolors='red', facecolors='none')
 
 plt.figure('True C-Scan')
-plt.imshow(data.c_scan, cmap='gray', extent=data.c_scan_extent)
+plt.imshow(data.c_scan, cmap='gray', extent=data.c_scan_extent,
+           interpolation='none')
 plt.xlabel('X Scan Location (mm)')
 plt.ylabel('Y Scan Location (mm)')
 plt.grid()
@@ -63,7 +88,7 @@ plt.colorbar()
 fig = plt.figure('Manufactured C-Scan')
 axis = fig.add_subplot(111)
 image = axis.imshow(made_c_scan, cmap='gray', extent=data.c_scan_extent,
-                    interpolation='bicubic')
+                    interpolation='none')
 axis.add_collection(p)
 plt.xlabel('X scan Location (mm)')
 plt.ylabel('Y Scan Location (mm)')
@@ -77,5 +102,24 @@ plt.title('Gaussian Profile')
 plt.xlabel('Radius (mm)')
 plt.ylabel('Amplitude')
 plt.grid()
+
+# now we want to determine the vertical spread of the beam
+A = max_amps.max()
+mu = 0
+sigma = 0.7
+p0 = (A, mu, sigma)
+popt, pcov = curve_fit(gaussian_1d, z, max_amps, p0)
+
+z_dense = np.linspace(-2.5, 2.5, 1001)
+best_fit = gaussian_1d(z_dense, a=max_amps.max(), mu=0, sigma=1)
+
+plt.figure('Vertical Beam Spread')
+plt.plot(z, max_amps, 'bo', label='Data')
+plt.plot(z_dense, best_fit, 'r', label='Best Fit')
+plt.title('Vertical Beam Spread')
+plt.xlabel('Height Relative to Focus (mm)')
+plt.ylabel('C-Scan Max Amplitude')
+plt.grid()
+plt.legend()
 
 plt.show()
